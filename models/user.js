f91a89c2afb081ff8249b3b4f141592ebf6482ef
sameerbairwa <sameerbairwa07@gmail.com>
2020-04-26 21:40:00 +0530

    user model created for mongoDB database

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userShema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    contact: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+module.exports = person = mongoose.model('myappusers', userShema);
