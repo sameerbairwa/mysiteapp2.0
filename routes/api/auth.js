f640cb124a1375dfc40aa269c7396ab7cc74d99f
sameerbairwa <sameerbairwa07@gmail.com>
2020-04-27 18:04:16 +0530

    created post route for signup using passport authenticate

---
+++ routes/api/auth.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const csrf = require('csurf');
+const session = require("express-session");
+const passport = require('passport');
+
+
+const csrfProtection = csrf();
+router.use(csrfProtection);
+
+//@type     post
+//@route    /api/auth/user/signup
+//@desc     router for signup for users
+//@access   PUBLIC
+
+router.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash: true
+}));
+
+module.exports = router;
