50d2d97f0655e5db45d5b1963ed24952ea36efb2
sameerbairwa <sameerbairwa07@gmail.com>
2020-06-05 21:32:19 +0530

    added

--- setup/myurl.js
+++ setup/myurl.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURL: "",
-    secret: ""
+    mongoURL: "mongodb+srv://sameer:sameer%40123@mflix-0wvxj.mongodb.net/mysite2?retryWrites=true&w=majority",
+    secret: "mysiteappsecret"
 }
